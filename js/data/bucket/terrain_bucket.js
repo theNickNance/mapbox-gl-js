5452dd897eb5641e8ad0fa0d6a4022647f5cd626
Molly Lloyd <molly@mapbox.com>
2016-12-21 10:26:33 -0500

    process array buffer into texture

--- js/data/bucket/terrain_bucket.js
+++ js/data/bucket/terrain_bucket.js
@@ -1,6 +1,7 @@
 const Bucket = require('../bucket');
 const createVertexArrayType = require('../vertex_array_type');
 const createElementArrayType = require('../element_array_type');
+const createTerrainArrayType = require('../terrain_array_type');
 const loadGeometry = require('../load_geometry');
 const {DEMPyramid, Level} = require('../../geo/dem_pyramid');
 const EXTENT = require('../extent');
@@ -19,10 +20,11 @@ const terrainInterface = {
         {property: 'terrain-illumination-alignment',type: 'Uint8'},
         {property: 'terrain-exaggeration',          type: 'Uint8'}
     ],
-    elementArrayType: createElementArrayType()
+    elementArrayType: createElementArrayType(),
+    terrainArrayType: createTerrainArrayType()
 }
 
-
+var log = true
 class TerrainBucket extends Bucket {
     constructor(options) {
         super(options, terrainInterface);
@@ -30,12 +32,26 @@ class TerrainBucket extends Bucket {
         this.terrainTile;
     }
 
+    addLevel(level){
+        const arrays = this.arrays;
+        for (var i = 0; i< level.data.length; i++) {
+            arrays.terrainArray.emplaceBack(level.data[i]);
+        }
+    }
+
     populate(features, options) {
         this.terrainTile = features[0];
         this.pyramid = this.getDEMPyramid();
+        if (this.pyramid && this.pyramid.loaded) {
+            for (var i=0; i< this.pyramid.levels.length; i++){
+                this.addLevel(this.pyramid.levels[i]);
+            }
+        }
     }
 
     getDEMPyramid(){
+        const arrays = this.arrays;
+        // console.log(this.terrainTile._pbf);
         let pyramid = new DEMPyramid();
         if (this.terrainTile) {
             if (this.terrainTile.extent != 256) {
@@ -44,21 +60,31 @@ class TerrainBucket extends Bucket {
             }
         }
 
-        let pbf= this.terrainTile._pbf;
+        let pbf = this.terrainTile._pbf;
         pbf.pos = this.terrainTile._geometry;
+
         // decode first level:
         pyramid.levels.push(new Level(256, 256, 128));
 
         let level = pyramid.levels[0];
-        for (let y = 0; y < level.height; y++) {
-            for (let x = 0; x < level.width; x++) {
-                const value = pbf.readSVarint();
-                const value_left = x ? level.getPixelValue(x - 1, y) : 0;
-                const value_up = y ? level.getPixelValue(x, y - 1) : 0;
-                const value_up_left = x && y ? level.getPixelValue(x - 1, y - 1) : 0;
-                level.setPixelValue(x, y, value + value_left + value_up - value_up_left);
+        for (var y = 0; y < level.height; y++) {
+            for (var x = 0; x < level.width; x++) {
+                var value = pbf.readSVarint();
+                var value_left = x ? level.get(x - 1, y) : 0;
+                var value_up = y ? level.get(x, y - 1) : 0;
+                var value_up_left = x && y ? level.get(x - 1, y - 1) : 0;
+                level.set(x, y, value + value_left + value_up - value_up_left);
             }
         }
+
+        pyramid.buildLevels();
+        pyramid.decodeBleed(pbf);
+        pyramid.loaded = true;
+        return pyramid;
+    }
+
+    isEmpty() {
+        return false;
     }
 }
 module.exports = TerrainBucket;
