2596a246c458442ccf9035e6f74ca9b91984a7ea
Molly Lloyd <molly@mapbox.com>
2017-05-12 11:01:50 -0700

    correct layer type, refactor tile cacheing a lil

--- src/render/painter.js
+++ src/render/painter.js
@@ -284,7 +284,6 @@ class Painter {
         if (layer.isHidden(this.transform.zoom)) return;
         if (layer.type !== 'background' && !coords.length) return;
         this.id = layer.id;
-
         draw[layer.type](painter, sourceCache, layer, coords);
     }
 
